b4d7f42344b2cfa25ce8d860a2ca62af68168b7c
jesusoyer <oyervidesjesus017@gmail.com>
2022-11-02 16:04:12 -0500

    initalized state for searchBar component and added eventlistener for on submit

---
+++ video_app/src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class SearchBar extends React.Component{
+state = {term: ''}
+
+//event callback arrow function
+onInputChange= event =>{
+this.setState({term: event.target.value})
+}
+
+onFormSubmit = event => {
+    event.preventDefault();
+    
+}
+
+    render(){
+        return  (
+            <div className="search-bar ui segment">
+                <form onSubmit = {this.onFormSubmit} className="ui form">
+                    <div className="field"></div>
+                    <label>Video Search</label>
+                    <input 
+                        type='text'
+                        value={this.state.term}
+                        onChange={this.onInputChange}
+                        />
+                            
+                        
+                </form>
+                </div>
+            )
+    }
+}
+
+export default SearchBar;
